e823ec517ed63bf7637c4e4cd13dec327bf9fa57
朱江晨@密歇根大学 <zjcsjtu@umich.edu>
2020-02-22 19:23:54 -0500

    Connected Yelp Fusion

--- api/api.js
+++ api/api.js
@@ -1,16 +1,128 @@
-
+const dataRestaurants = require('../data/restaurants.json').restaurants;
+const yelp = require('yelp-fusion');
+const apiKey = process.env.YELP_API_KEY;
+const client = yelp.client(apiKey);
 module.exports = class Api {
     constructor() {
 
     }
 
-    // arguments and return values undecided
-    yelpFusion() {
-
+    yelpFormatButton(result) {
+        return {
+            "title": result.name,
+            "image_url": result.image_url,
+            "subtitle": '...',
+            "default_action": {
+                "type": "web_url",
+                "url": result.url,
+                "webview_height_ratio": "tall"
+            },
+            "buttons": [
+                {
+                    "type": "postback",
+                    "title": "Select it",
+                    "payload": "SELECT_IT"
+                },
+                {
+                    "type": "postback",
+                    "title": "See details",
+                    "payload": "SEE_DETAILS"
+                },
+                {
+                    "type": "postback",
+                    "title": "Remove it",
+                    "payload": "REMOVE_IT"
+                }
+            ]
+        };
     }
 
     // arguments and return values undecided
-    clincFusion() {
+     yelpFusion(require) {
+        // const searchRequest = {
+        //     term: require,
+        //     location: 'ann arbor, mi',
+        //     limit: 10
+        // };
+        // var restaurants = []
+        // // const request = async () => {
+        //  client.search(searchRequest).then(response => {
+        //      const firstResult = response.jsonBody.businesses;
+        //     //  const prettyJson = JSON.parse(JSON.stringify(firstResult));
+        //      console.log(firstResult);
+        //     //  console.log(Array.from(prettyJson).length);
+             
+        //      (firstResult).forEach((result) => {
+        //         //  console.log('result')
+        //         //  console.log(result)
+        //         restaurants = restaurants.concat([this.yelpFormatButton(result)]);
+        //          console.log(restaurants.length);
 
+        //     });
+        //      console.log(restaurants.length);
+        //  }).catch(e => {
+        //      console.log(e);
+        //  });
+        //  var count = 0;
+        //  console.log(restaurants.length);
+        //  while (restaurants.length == 0) {
+        //     count += 0.001;
+        //     // if (count > 10000) {
+        //     //     break;
+        //     // }
+        //     //  console.log(restaurants.length);
+        //     //  console.log(count);
+        //  }
+        //  console.log(restaurants.length);
+
+        // return restaurants;
+        // // }
+        // // return request();
+        // // await client.search(searchRequest);
+
+        // //     const firstResult = response.jsonBody.businesses[0];
+        // //     const prettyJson = JSON.stringify(firstResult, null, 4);
+        // //     console.log(prettyJson);
+        // //     prettyJson.forEach((result) => {
+        // //         restaurants.concat(this.yelpFormatButton(result));
+        // //     })
+        // //     return restaurants;
+
+        // // var x = await client.search(searchRequest).then(response => {
+        // //     const firstResult = response.jsonBody.businesses;
+        // //     const prettyJson = JSON.stringify(firstResult);
+        // //     console.log(prettyJson);
+        // //     // Array.from(prettyJson).forEach((result) => {
+        // //     //     restaurants.concat(this.yelpFormatButton(result));
+        // //     // });
+        // //     return dataRestaurants;
+        // // }).catch(e => {
+        // //     console.log(e);
+        // // })
+        // // // var response = await client.search(searchRequest);
+        // // await Promise.all(x);
+        // hard-coded
+        return dataRestaurants;
+    }
+
+
+    // arguments and return values undecided
+    clinc(userInput) {
+        if (userInput.startsWith('remove ')) {
+            var pos = parseInt(userInput.slice(7));
+            return { 'intent': 'remove', 'position': pos }
+        } else if (userInput.startsWith('see details ')) {
+            var pos = parseInt(userInput.slice(12));
+            return { 'intent': 'detail', 'position': pos }
+        } else if (userInput.startsWith('find ')) {
+            return { 'intent': 'find', 'require': userInput.slice(5) };
+        } else if (userInput.startsWith('select ')) {
+            var pos = parseInt(userInput.slice(7));
+            return { 'intent': 'select', 'position': pos }
+        } else if (userInput.startsWith('help ')) {
+            return { 'intent': 'help', 'require': userInput.slice(5) };
+        } else {
+            return { 'intent': 'outofscope' };
+        }
     }
 }
--- core/chatbot.js
+++ core/chatbot.js
@@ -7,26 +7,99 @@ const dataRestaurants = require('../data/restaurants.json').restaurants;
 // var restaurants = JSON.parse(JSON.stringify(dataRestaurants));
 // utils.setPayload(restaurants);
 
+const yelp = require('yelp-fusion');
+const apiKey = process.env.YELP_API_KEY;
+const client = yelp.client(apiKey);
+
 // include all APIs.
 var api = new Api();
+
+const states = {
+    ROOT: 'root',
+    FIND: 'find',
+    DECIDE: 'decide'
+};
 
 module.exports = class Chatbot {
     constructor() {
         // default entry state is called root.
-        this.state = 'root';
+        this.state = states.ROOT;
+        this.lastInteractionTime = Date.now();
+        this.dataRestaurants = []
+        this.restaurants = [];
+        utils.setPayload(this.restaurants);
+    }
+
+    restart() {
+        // return to root state and re-initialize hardcoded data.
+        this.state = states.ROOT;
         this.lastInteractionTime = Date.now();
         this.dataRestaurants = dataRestaurants;
         this.restaurants = JSON.parse(JSON.stringify(this.dataRestaurants));
         utils.setPayload(this.restaurants);
     }
 
-    restart() {
-        // return to root state and re-initialize hardcoded data.
-        this.state = 'root';
-        this.lastInteractionTime = Date.now();
-        this.dataRestaurants = dataRestaurants;
-        this.restaurants = JSON.parse(JSON.stringify(this.dataRestaurants));
-        utils.setPayload(this.restaurants);
+    yelpFormatButton(result) {
+        return {
+            "title": result.name,
+            "image_url": result.image_url,
+            "subtitle": '...',
+            "default_action": {
+                "type": "web_url",
+                "url": result.url,
+                "webview_height_ratio": "tall"
+            },
+            "buttons": [
+                {
+                    "type": "postback",
+                    "title": "Select it",
+                    "payload": "SELECT_IT"
+                },
+                {
+                    "type": "postback",
+                    "title": "See details",
+                    "payload": "SEE_DETAILS"
+                },
+                {
+                    "type": "postback",
+                    "title": "Remove it",
+                    "payload": "REMOVE_IT"
+                }
+            ]
+        };
+    }
+
+    // arguments and return values undecided
+    yelpFusion(require, sender_psid) {
+        const searchRequest = {
+            term: require,
+            location: 'ann arbor, mi',
+            limit: 10
+        };
+        // const request = async () => {
+         client.search(searchRequest).then(response => {
+             const results = response.jsonBody.businesses;
+             this.dataRestaurants = JSON.parse(JSON.stringify(results));
+             results.forEach((result) => {
+                this.restaurants = this.restaurants.concat([this.yelpFormatButton(result)]);
+            });
+             console.log(this.restaurants.length);
+             utils.setPayload(this.restaurants);
+             response = {
+                 "attachment": {
+                     "type": "template",
+                     "payload": {
+                         "template_type": "generic",
+                         "elements": this.restaurants.slice(0, 3)
+                     }
+                 }
+             }
+             this.callSendAPI(sender_psid, response);
+         }).catch(e => {
+             console.log(e);
+         });
+        // hard-coded
+        // return dataRestaurants;
     }
 
     handleMessage(sender_psid, received_message) {
@@ -34,26 +107,87 @@ module.exports = class Chatbot {
 
         // Checks if the message contains text
         if (received_message.text) {
-            // Create the payload for a basic text message, which
-            // will be added to the body of our request to the Send API
+
             response = {
-                "text": `You sent the message: "${received_message.text}". Here are the recommended restaurants!`
+                "text": `You sent the message: "${received_message.text}".`
             }
             // Send the response message
             this.callSendAPI(sender_psid, response);
 
-            this.restaurants = JSON.parse(JSON.stringify(this.dataRestaurants));
-            utils.setPayload(this.restaurants);
-            response = {
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "generic",
-                        "elements": this.restaurants
+            var userInput = api.clinc(received_message.text);
+
+            if (this.state == states.ROOT) {
+                // if user wants to find restaurant, state -> FIND
+                // and displays three restaurants returned by Yelp fusion
+                if (userInput.intent == 'find') {
+                    this.restaurants = [];
+                    this.dataRestaurants = [];
+                    this.yelpFusion(userInput.require, sender_psid);
+                    this.state = states.FIND;
+                } else {
+                    // if user says any other intents, stay in ROOT, ask again
+                    response = {
+                        "text": 'Please say your requirements'
                     }
+                    this.callSendAPI(sender_psid, response);
+
                 }
+            } else if (this.state == states.FIND) {
+                // if user wants to change search filter, then re-call Yelp,
+                // and display new three restaurants
+                if (userInput.intent == 'find') {
+                    this.restaurants = [];
+                    this.dataRestaurants = [];
+                    this.yelpFusion(userInput.require, sender_psid);
+                } else if (userInput.intent == 'remove') {
+                    // if user removes one restaurant, display one new from the ones returned by Yelp API
+                    var idx = userInput.position;
+                    response = { 'text': 'Removing ' + this.restaurants[idx].title + '... New list is below' };
+                    this.callSendAPI(sender_psid, response);
+                    this.restaurants.splice(idx, 1);
+                    this.dataRestaurants.splice(idx, 1);
+                    utils.setPayload(this.restaurants);
+                    response = {
+                        "attachment": {
+                            "type": "template",
+                            "payload": {
+                                "template_type": "generic",
+                                "elements": this.restaurants.slice(0, 3)
+                            }
+                        }
+                    };
+                    this.callSendAPI(sender_psid, response);
+                } else if (userInput.intent == 'detail') {
+                    // if user see details, display details.
+                    var idx = userInput.position;
+                    // response = { 'text': this.restaurants[idx].title + ' is the best restaurant!!!' };
+                    var name = JSON.stringify(this.dataRestaurants[idx].name);
+                    var rating = JSON.stringify(this.dataRestaurants[idx].rating);
+                    var price = JSON.stringify(this.dataRestaurants[idx].price);
+                    var location = JSON.stringify(this.dataRestaurants[idx].location);
+                    response = {'text': `Name: ${name}\nRating: ${rating}\nPrice: ${price}\nLocation: ${location}`}
+                    this.callSendAPI(sender_psid, response);
+                } else if (userInput.intent == 'select') {
+                    // if user selects, change state to DECIDE, save what he selects, ask if they need further help.
+                    var idx = userInput.position;
+                    response = { 'text': this.restaurants[idx].title + ' is selected. Do you need other help?' };
+                    this.callSendAPI(sender_psid, response);
+                    this.state = states.DECIDE;
+                }              
+            } else if (this.state == states.DECIDE) {
+                // if user requests reservation, etc, then stay in DECIDE
+                if (userInput.intent == 'help') {
+                    response = { 'text': 'Did some help: ' + userInput.require };
+                    this.callSendAPI(sender_psid, response);
+                }
+                else{
+                    // else return ROOT
+                    this.state = states.ROOT;
+                    response = { 'text': 'ByeBye, return to ROOT.' };
+                    this.callSendAPI(sender_psid, response);   
+                }
+                
             }
-            this.callSendAPI(sender_psid, response);
 
         } else if (received_message.attachments) {
             // Get the URL of the message attachment
@@ -156,4 +290,6 @@ module.exports = class Chatbot {
             }
         });
     }
+
+  
 }
